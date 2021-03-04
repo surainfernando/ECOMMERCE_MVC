1713e540b54390ab2840344c5bd2dfd6294e358b
Surain Fernando <surainnewfernando@gmail.com>
2021-03-04 17:03:10 +0530

    Add Ctegory UI

--- ECOMMERCE_MVC/Controllers/AdminController.cs
+++ ECOMMERCE_MVC/Controllers/AdminController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using ECOMMERCE_MVC.Models;
 
 namespace ECOMMERCE_MVC.Controllers
 {
@@ -22,21 +23,73 @@ namespace ECOMMERCE_MVC.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create()
+        public IActionResult Index(Admin a)
         {
-            FirstInsert();
+           int s=FirstInsert(a);
+            if (s == 1) { return RedirectToAction("ViewProducts"); }
+            else if (s == 0)
+            {
+                ModelState.AddModelError("Email", "The Email is incorrect");
+                return View();
+            }
+            else {
+                ModelState.AddModelError("Password", "The  Password is incorrect");
+                return View();
 
+            }
+
+            
+        }
+
+        public IActionResult ViewProducts()
+        {
             return View();
         }
-        public void FirstInsert()
+
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+
+
+
+        public int FirstInsert(Admin a)
         {
            // SqlConnection connection = new SqlConnection();
            // connection.ConnectionString= $@"Data Source=LAPTOP-FAS31PRJ;Initial Catalog=EcommerceBook;User Id=LAPTOP-FAS31PRJ\surai;Password=;Integrated Security=true";
             _connection.Open();
-            string query = "insert into a(id,name) values(8,'77777uior')";
+            //String email = a.Email;
+            String password = a.Password;
+            string query = $"select * from adminprofile where email='{a.Email}'";
+            Console.WriteLine(query);
             SqlCommand command = new SqlCommand(query,_connection);
-            command.ExecuteNonQuery();
-        
+            SqlDataReader dataReader = command.ExecuteReader();
+                 string dbEmail = "";
+                string dbPassword = ""; 
+            while (dataReader.Read())
+            {
+                dbEmail = (string)dataReader["Email"];
+                dbPassword = (string)dataReader["Password"];
+            }
+
+            if (dbEmail == "")
+            {
+                return 0;
+            }
+            else {
+                if (dbPassword == password)
+                {
+                    return 1;
+
+                }
+                else {
+                    return 2;
+                
+                }
+            }
+
+
         }
 
     }
