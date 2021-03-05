1cba001e5c311c48a9af8928517e7ae5811f09a0
Surain Fernando <surainnewfernando@gmail.com>
2021-03-05 23:34:29 +0530

    User Login and Sign Up changes

---
+++ ECOMMERCE_MVC/Controllers/Customer1Controller.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECOMMERCE_MVC.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ECOMMERCE_MVC.Controllers
+{
+    public class Customer1Controller : Controller
+    {
+        public readonly SqlConnection _connection;
+
+        public Customer1Controller(SqlConnection con)
+        {
+            _connection = con;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(ECOMMERCE_MVC.Models.Customer a)
+        {
+            int s= ECOMMERCE_MVC.Models.Customer.LoginDB(a, _connection);
+            
+            if (s == 1) { return RedirectToAction("Index","Item1"); }
+            else if (s == 0)
+            {
+                ModelState.AddModelError("Email", "The Email is incorrect");
+                return View();
+            }
+            else
+            {
+                ModelState.AddModelError("Password", "The  Password is incorrect");
+                return View();
+
+            }
+
+        }
+
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(Models.Customer a)
+        {
+            int d = ECOMMERCE_MVC.Models.Customer.InsertCustomer(a,_connection);
+            return RedirectToAction("Index","Item1");
+        }
+
+
+
+
+    }
+}
---
+++ ECOMMERCE_MVC/Models/Customer.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECOMMERCE_MVC.Models
+{
+    public class Customer
+    {
+        public Customer()
+        { 
+
+        }
+        [Key]
+        public int Id { get; set; }
+
+
+
+        [Required]
+        public String Email { get; set; }
+
+
+        [Required]
+        public string Password { get; set; }
+
+        [NotMapped] // Does not effect with your database
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public String Name { get; set; }
+
+        [Required]
+        public String Address { get; set; }
+
+        public static int InsertCustomer(Customer a, SqlConnection _connection)
+        {
+            _connection.Open();
+           // string query = $"Insert into customer(name) values('y')";
+
+            string query = $"Insert into Customer(name,email,password,address) values('{a.Name}','{a.Email}','{a.Password}','{a.Address}')";
+            SqlCommand command = new SqlCommand(query, _connection);
+            int rows = command.ExecuteNonQuery();
+            _connection.Close();
+            return rows;
+
+
+
+        }
+
+        public static int LoginDB(Customer a, SqlConnection _connection)
+        {
+            // SqlConnection connection = new SqlConnection();
+            // connection.ConnectionString= $@"Data Source=LAPTOP-FAS31PRJ;Initial Catalog=EcommerceBook;User Id=LAPTOP-FAS31PRJ\surai;Password=;Integrated Security=true";
+            _connection.Open();
+            //String email = a.Email;
+            String password = a.Password;
+            string query = $"select * from customer where email='{a.Email}'";
+            Console.WriteLine(query);
+            SqlCommand command = new SqlCommand(query, _connection);
+            SqlDataReader dataReader = command.ExecuteReader();
+            string dbEmail = "";
+            string dbPassword = "";
+            while (dataReader.Read())
+            {
+                dbEmail = (string)dataReader["Email"];
+                dbPassword = (string)dataReader["Password"];
+            }
+
+            if (dbEmail == "")
+            {
+                return 0;
+            }
+            else
+            {
+                if (dbPassword == password)
+                {
+                    return 1;
+
+                }
+                else
+                {
+                    return 2;
+
+                }
+            }
+
+
+        }
+    }
+}
